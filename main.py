0859b0250c69ac10c576afc48a55ab1084c11dee
Raymond Watkin <raymonduscw@gmail.com>
2021-10-24 19:32:11 -0400

    added TensorBoard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from alexnet import create_alexnet
 from data import data_generator
 from tensorflow.keras.utils import plot_model
+from tensorflow.keras.callbacks import TensorBoard
 import sys
 import random
 
@@ -13,6 +14,9 @@ if __name__ == '__main__':
 
     model = create_alexnet()
     plot_model(model, to_file="model.png")
+
+    tb = TensorBoard(log_dir="tf_logs", histogram_freq=0, write_graph=True)
+    tb.set_model(model)
 
     for it_epoch in range(30):
         for it_batch, batch in enumerate(data_generator(traindir, 128)):
