4d77ec2e41b0ccfebdd106924bffed8589a2fd4f
Raymond Watkin <raymonduscw@gmail.com>
2021-10-24 20:04:00 -0400

    added image_dataset_from_directory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from alexnet import create_alexnet
-from data import data_generator
-from tensorflow.keras.utils import plot_model
+from tensorflow.keras.utils import plot_model, image_dataset_from_directory
 import sys
 import random
 
@@ -13,16 +12,17 @@ if __name__ == '__main__':
 
     model = create_alexnet()
     plot_model(model, to_file="model.png")
+    X_train, Y_train = image_dataset_from_directory(traindir, image_size= (224, 224), seed=10)
+    model.fit(X_train, Y_train, epochs= 30)
 
-    for it_epoch in range(30):
-
-        for it_batch, batch in enumerate(data_generator(traindir, 128)):
-
-            X_train, Y_train = batch
-            is_first_batch = it_batch == 0
-            metrics = model.train_on_batch(X_train, Y_train, reset_metrics = is_first_batch, return_dict = True)
-            #print("Epoch Number:", it_epoch, "Accuracy:", acc, "Mean Square Error:", mse)
-
-            print("Epoch Number:", it_epoch, "Metrics:", metrics)
+    # for it_epoch in range(30):
+    #     for it_batch, batch in enumerate(data_generator(traindir, 128)):
+    #
+    #         X_train, Y_train = batch
+    #         is_first_batch = it_batch == 0
+    #         metrics = model.train_on_batch(X_train, Y_train, reset_metrics = is_first_batch, return_dict = True)
+    #         #print("Epoch Number:", it_epoch, "Accuracy:", acc, "Mean Square Error:", mse)
+    #
+    #         print("Epoch Number:", it_epoch, "Metrics:", metrics)
 
 
